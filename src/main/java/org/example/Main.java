5c74e566e4d50a7d4b5c5784ccb0d81aa9b668bf
Conner Gleason <connereg@optonline.net>
2022-08-04 14:30:04 -0400

    Completed Lab

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.example.models.Capital;
+import org.example.models.Country;
+
 import javax.persistence.Persistence;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityManager;
@@ -13,6 +16,20 @@ public class Main {
 
         // access transaction object
         EntityTransaction transaction = entityManager.getTransaction();
+
+
+        Capital newCapital = new Capital();
+        newCapital.setName("Washington DC");
+
+        Country newCountry = new Country();
+        newCountry.setName("USA");
+
+        newCountry.setCapital(newCapital);
+
+        transaction.begin();
+        entityManager.persist(newCountry);
+        entityManager.persist(newCapital);
+        transaction.commit();
 
         // close entity manager
         entityManager.close();
